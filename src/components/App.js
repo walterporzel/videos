ba6c86367ab9f2c45ae47b085f2690f67bd3bd96
Walter_Porzel <walterp@vt.edu>
2021-07-10 18:32:50 -0400

    modified readme

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Searchbar from "./SearchBar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+
+class App extends React.Component {
+  state = { videos: [] };
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+
+    this.setState({ videos: response.data.items });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <Searchbar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos}/>
+      </div>
+    );
+  }
+}
+
+export default App;
