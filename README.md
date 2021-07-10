d33b183e19344e7e72c2950dac55f3fcb2a67f8e
Walter_Porzel <walterp@vt.edu>
2021-07-10 19:28:30 -0400

    completed initial version of app

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # Video Player
 
-This application is a simplified version of the youtube video browser
+This application is a simplified version of the youtube video browser. It allows a user to enter a search term, connects to the [youtube v3 api](https://developers.google.com/youtube/v3/docs) to return the top 5 video results for the indicated term, and allows the user to play videos directly from the webpage
+
+![Flow from components](./public/Webpage.png)
 
 ## Component Flow
 
 ![Flow from components](./public/Diagram1.png)
+
+## More Detailed component flow
+
+![Flow from components](./public/Diagram2.png)
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import VideoItem from './VideoItem';
+import React from "react";
+import VideoItem from "./VideoItem";
 
-const VideoList = ({ videos }) => {
-    const renderedList = videos.map((video) => {
-        return <VideoItem video={video} />;
-    });
+const VideoList = ({ videos, onVideoSelect }) => {
+  const renderedList = videos.map((video) => {
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        onVideoSelect={onVideoSelect}
+        video={video}
+      />
+    );
+  });
 
-    return <div className="ui relaxed divided list">{renderedList}</div>
+  return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
